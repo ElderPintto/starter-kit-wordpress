ed911dd8d46a41c6667e56e7f1dfa6a3e8ec4a50
Mac Giovanni <maclevison@gmail.com>
2018-01-22 10:39:22 -0300

    Add config files

---
+++ public/wp-config.php
@@ -0,0 +1,46 @@
+<?php
+ini_set( 'display_errors', 0 );
+
+// ===================================================
+// Load database info and local development parameters
+// ===================================================
+include( dirname( __FILE__ ) . '/../.env' );
+
+// ========================
+// Custom Content Directory
+// ========================
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content' );
+
+// ================================================
+// You almost certainly do not want to change these
+// ================================================
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+// ================================
+// Language
+// Leave blank for American English
+// ================================
+define( 'WPLANG', 'pt_BR' );
+
+// ======================
+// Hide errors by default
+// ======================
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'WP_DEBUG', false );
+
+// =========================
+// Disable automatic updates
+// =========================
+define( 'AUTOMATIC_UPDATER_DISABLED', false );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+}
+require_once( ABSPATH . 'wp-settings.php' );
+
+// =======================
+// dont download plugin for admin
+// =======================
+define('DISALLOW_FILE_MODS',true);
